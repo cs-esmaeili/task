158dda877b97aff71ee33b9cb18a9e01be9e8ff5
Javad Esmaeili <cs.esmaeili@gmail.com>
2025-08-07 21:33:27 +0330

    feat(auth): add image to dashboard page

--- components/WelcomeMessage.tsx
+++ components/WelcomeMessage.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import { useLogout } from '@/hooks/useLogout'
 import styles from './WelcomeMessage.module.scss'
 import Button from './Button';
@@ -8,6 +9,7 @@ import Button from './Button';
 const WelcomeM = () => {
 
     const [fullName, setFullName] = useState<string>("");
+    const [imageUrl, setImageUrl] = useState<string>("");
     const logout = useLogout();
 
     useEffect(() => {
@@ -15,13 +17,22 @@ const WelcomeM = () => {
 
         if (storedUser) {
             const user = JSON.parse(storedUser);
-            setFullName((user.results[0].name.first + user.results[0].name.last) || "");
+            setFullName((user.results[0].name.first + " " + user.results[0].name.last) || "");
+            setImageUrl(user.results[0].picture.medium || "");
         }
     }, []);
 
-
     return (
         <div className={styles.container}>
+            {imageUrl && (
+                <Image
+                    src={imageUrl}
+                    alt={fullName}
+                    width={48}
+                    height={48}
+                    className={styles.avatar}
+                />
+            )}
             <h2>👋 Welcome, {fullName}</h2>
 
             <Button onClick={logout}>
