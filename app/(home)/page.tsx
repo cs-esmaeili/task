36a1d8571086ab683993ac28e6ef02346b9a67c9
Javad Esmaeili <cs.esmaeili@gmail.com>
2025-08-07 17:36:06 +0330

    feat(ui): Home page style changed

---
+++ app/(home)/page.tsx
@@ -0,0 +1,28 @@
+import styles from './Home.module.scss';
+import { FaHome } from "react-icons/fa";
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+      <div className={styles.box}>
+        <div className={styles.titleWrapper}>
+          <FaHome className={styles.icon} />
+          <h1 className={styles.title}>Home Page</h1>
+
+          <Link href="/auth">
+            <span>
+              Login Page
+            </span>
+          </Link>
+
+        </div>
+      </div>
+
+      <div className={styles.blurCircle1}></div>
+      <div className={styles.blurCircle2}></div>
+      <div className={styles.blurCircle3}></div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="body">
         {children}
       </body>
     </html>
